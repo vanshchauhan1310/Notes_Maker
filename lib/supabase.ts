8fcc407151353b49cf085652f5e50e50c6fb52e4
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2025-07-15 14:46:45 +0530

    fix some bug v2

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
-import { Database } from './database.types'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL 
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY 
 
-export const supabase = createClient<Database>(
+export const supabase = createClient(
   supabaseUrl!,
   supabaseAnonKey!
 )
